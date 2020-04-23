3046787a3375500360fc1da4b820853b8d5deb02
Muguku <hello@muguku.co.ke>
2020-04-24 02:56:19 +0300

    conditionally render logged in and logged out components and show random quotes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// import the custom hook
+import { useLogin } from "./utils/useLogin";
+
+// import Logged in and Logged out components
+import LoggedIn from "./components/LoggedIn";
+import LoggedOut from "./components/LoggedOut";
+
+// function that returns a random quote object
+// {author:"foo", quote:"bar"}
+import { getRandomQuote } from "./utils/getRandomQuote";
 
 function App() {
+  const [Quote, setQuote] = useState({});
+  // de-structure to get the hook methods and objects
+  const { loggedIn, loading, logInAndOut } = useLogin();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* show the quote if available (object has size > 0) and a blank space if not */}
+        <label>
+          {Object.keys(Quote).length > 0
+            ? `${Quote.quote} ~ ${Quote.author}`
+            : ""}
+        </label>
+        {/* on click get a random quote from the random quote generator and set it to state */}
+        <button
+          style={{ marginTop: "10px" }}
+          onClick={() => {
+            // get random quote
+            let randomQuote = getRandomQuote();
+            // set to state
+            setQuote(randomQuote);
+          }}
         >
-          Learn React
-        </a>
+          Generate A Random Quote
+        </button>
+
+        {/* if logged in is true show logged in component else show logged out component */}
+        {/* Read about the ternary operator here -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator */}
+        {loggedIn === true ? (
+          <LoggedIn logInAndOut={logInAndOut} loading={loading} />
+        ) : (
+          <LoggedOut logInAndOut={logInAndOut} loading={loading} />
+        )}
       </header>
     </div>
   );
